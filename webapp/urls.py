d7e3e7d35856f256f41fe8baa1a44e1147357d29
Denise Leviste <drleviste@dad-l-02-linux.lan.dream.upd.edu.ph>
2016-04-16 23:07:21 +0800

    Login modal + search result page + urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    # url(r'^$', views.home, name='home'),
+   	url(r'^/?$', TemplateView.as_view(template_name='home.html'), name='home'),
+   	url(r'^search/$', TemplateView.as_view(template_name='search.html'), name='search'),
+    url(r'^register/$', TemplateView.as_view(template_name='register.html'), name='register'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^recipients/$', views.rec, name='rec'),
     url(r'^recipients/add$', views.addrec, name='addrec'),
